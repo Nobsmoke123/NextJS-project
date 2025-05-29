df80b823046c6054e5f788c38ba1f0f3fe84d886
Nobsmoke123 <donaldakobundu@gmail.com>
2025-05-29 01:37:45 +0100

    feat: integrate Sanity Studio and related configurations
    - Added Sanity Studio route at `/app/studio/[[...tool]]/page.tsx`.
    - Created StartupCard component for displaying startup information.
    - Configured Sanity CLI with project ID and dataset in `sanity.cli.ts`.
    - Defined Sanity Studio configuration in `sanity.config.ts` with plugins.
    - Set up environment variables for Sanity API version, dataset, and project ID in `sanity/env.ts`.
    - Implemented Sanity client in `sanity/lib/client.ts`.
    - Added image URL builder in `sanity/lib/image.ts`.
    - Established live content fetching in `sanity/lib/live.ts`.
    - Defined initial schema types in `sanity/schemaTypes/index.ts`.
    - Created content structure for Sanity Studio in `sanity/structure.ts`.
    - Updated `package.json` with new dependencies for Sanity integration.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,26 @@
 import SearchForm from "@/components/SearchForm";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 
 export default async function Home({
   searchParams,
 }: {
   searchParams: Promise<{ query?: string }>;
 }) {
-
   const query = (await searchParams).query || "";
+
+  const posts: Array<StartupTypeCard> = [
+    {
+      _id: 1,
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: "Chidera Ejimofor" },
+      description: "This is a description",
+      image:
+        "https://images.unsplash.com/photo-1742827871494-3a34fc06b69f?q=80&w=1984&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      category: "Robots",
+      title: "We Robots",
+    },
+  ];
 
   return (
     <>
@@ -20,7 +34,23 @@ export default async function Home({
           Competitions.
         </p>
 
-        <SearchForm query={query}/>
+        <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post, index) => (
+              <StartupCard key={post._id} post={post} />
+            ))
+          ) : (
+            <p className="text-20-semibold text-center">No Startups Found</p>
+          )}
+        </ul>
       </section>
     </>
   );
